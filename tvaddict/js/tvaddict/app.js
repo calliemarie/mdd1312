be00129e72882a3f0cc056bf5f065c296364a0cf
Callie Kavourgias <calliemariek@gmail.com>
2014-02-01 18:28:04 -0500

    Completely working
    Completely working project.  Currently links out to trakt.tv for show
    details, will try to fix if there's time.

--- tvaddict/js/tvaddict/app.js
+++ tvaddict/js/tvaddict/app.js
@@ -1,1 +1,10 @@
 var app = angular.module('TVAddictApp',[]);
+
+//config([ '$routeProvider', function($routeProvider){
+
+//	$routeProvider
+//		when("/shows", {templateUrl: "partials/shows.html", controller: "mainController"}).
+//		when("/show/:id", {templateUrl: "partials/show.html", controller: "mainController"}).
+//		otherwise({redirectTo:'/shows'});
+	
+//}])
